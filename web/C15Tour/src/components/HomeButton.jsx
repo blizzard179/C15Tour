01c95141067ef9ff1000e4dfa375b6bb76202902
Godofwoods <tomasmartineau@gmail.com>
2026-01-05 12:01:26 +0100

    Ajoute Bouton Home

---
+++ web/C15Tour/src/components/HomeButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomeButton = () => {
+    const handleClick = () => {
+        window.location.href = '/';
+    };
+
+    return (
+        <button 
+            onClick={handleClick}
+            className="buttonHome"
+        >
+            <img src="/src/assets/picto/boutton_home.svg" alt="Home" />
+        </button>
+    );
+};
+
+export default HomeButton;
