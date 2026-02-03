b9d0c465a1a2b67cd32bdcceb8200349f27d569a
blizzard179 <77052500+blizzard179@users.noreply.github.com>
2026-02-03 11:05:27 +0100

    reset du projet mobile C15Tour-mobile

---
+++ mobile/C15Tour-mobile/components/ui/scroll-up/index.tsx
@@ -0,0 +1,11 @@
+import { ThemedView } from "@/components/themed-view";
+import LoginHeader from "./login-header";
+
+function  ScrollUp() {
+    return (
+        <ThemedView>
+            <LoginHeader />
+        </ThemedView>
+    );
+} 
+export default ScrollUp;
---
+++ mobile/C15Tour-mobile/components/ui/scroll-up/login-header.tsx
@@ -0,0 +1,14 @@
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+
+function LoginHeader() {
+    return (
+        <ThemedView>
+            <ThemedText>
+                CHARGER UN ITINÉRAIRE
+            </ThemedText>
+        </ThemedView>
+    );
+}
+
+export default LoginHeader;
