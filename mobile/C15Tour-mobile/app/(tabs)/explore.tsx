04b38a400c9d01257161ddb9ba485e7ee07ba5fa
Anthony Nguyen <anthonydojolotusstudio@gmail.com>
2026-04-13 15:24:22 +0200

    feat: add leader and participant icons

--- mobile/C15Tour-mobile/app/(tabs)/explore.tsx
+++ mobile/C15Tour-mobile/app/(tabs)/explore.tsx
@@ -1,5 +1,7 @@
 import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import CursorVehicule from '../../../../shared/global_assets/pictos/CursorVehicule.svg';
+import CursorVehiculeLeader from '../../../../shared/global_assets/pictos/CursorVehiculeLeader.svg';
 
 const urlOpenStreetView =
   'https://www.openstreetmap.org/export/embed.html?bbox=-1.595%2C47.196%2C-1.505%2C47.237&layer=mapnik';
@@ -8,6 +10,13 @@ export default function ExploreScreen() {
   return (
     <View style={styles.container}>
       <WebView source={{ uri: urlOpenStreetView }} style={styles.webview} />
+
+      <View style={styles.cursorVehicule}>
+        <CursorVehicule width={40} height={40} />
+      </View>
+      <View style={styles.cursorVehiculeLeader}>
+        <CursorVehiculeLeader width={40} height={40} />
+      </View>
     </View>
   );
 }
@@ -19,4 +28,16 @@ const styles = StyleSheet.create({
   webview: {
     flex: 1,
   },
+  cursorVehicule: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [{ translateX: -20 }, { translateY: -20 }],
+  },
+  cursorVehiculeLeader: {
+    position: 'absolute',
+    top: '30%',
+    left: '50%',
+    transform: [{ translateX: -20 }, { translateY: -20 }],
+  }
 });
