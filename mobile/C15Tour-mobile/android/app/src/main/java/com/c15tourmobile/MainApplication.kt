6a770bb366fdf1a61a1d79bd1a7749925975adfa
Anthony Nguyen <anthonydojolotusstudio@gmail.com>
2026-06-15 21:51:43 +0200

    fix: repare files for android expo/studio

--- mobile/C15Tour-mobile/android/app/src/main/java/com/c15tourmobile/MainApplication.kt
+++ mobile/C15Tour-mobile/android/app/src/main/java/com/c15tourmobile/MainApplication.kt
@@ -5,10 +5,10 @@ import android.content.res.Configuration
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.ReactHost
 import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
